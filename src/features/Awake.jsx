10af72a961b776cdc630bf9710e067ac3009289a
Zesk <nhatkhang25092005@gmail.com>
2025-11-05 08:45:25 +0700

    fix name file2

--- src/features/Awake.jsx
+++ src/features/Awake.jsx
@@ -1,4 +1,4 @@
-import awake from "../services/api/Awake";
+import awake from "../services/api/awake.js";
 import { useEffect } from "react";
 export default function Awake(){
     useEffect(() => {
