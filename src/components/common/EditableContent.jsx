04313ede14ae20fcd04e332744a030a8e67f3fe0
Zesk <nhatkhang25092005@gmail.com>
2025-12-01 07:00:32 +0700

    fix see more in comment

--- src/components/common/EditableContent.jsx
+++ src/components/common/EditableContent.jsx
@@ -190,13 +190,13 @@ export default function EditableContent({
                 pl:"0.5rem",
                 pr:'0.5rem',
                 width: "100%",
+                ...nonEditStyle,
                 ...(!isExpanded && {
                     display: '-webkit-box',
                     WebkitLineClamp: 10,
                     WebkitBoxOrient: 'vertical',
                     overflow: 'hidden',
                 }),
-                ...nonEditStyle
             }}
                 ref={contentRef}
             >
