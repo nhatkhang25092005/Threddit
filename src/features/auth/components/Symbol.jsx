b3d7722bba4c6fb687e3ecc53009698a4472fe92
Zesk <nhatkhang25092005@gmail.com>
2026-04-12 12:12:29 +0700

    adjust logo

--- src/features/auth/components/Symbol.jsx
+++ src/features/auth/components/Symbol.jsx
@@ -3,7 +3,6 @@ import Logo from "../../../components/common/Logo"
 
 const style = {
   width: "100%",
-  height: "100vh",
   display: "flex",
   flexDirection: "column",
   justifyContent: "center",
